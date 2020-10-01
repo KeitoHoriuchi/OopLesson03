c65aad91c1ffd67a922bac917ed381e8b11caff0
KeitoHoriuchi <31110@ojs.ac.jp>
2020-10-01 10:39:03 +0900

    P117 リスト4.28

--- OopLesson03/Chapter4/Program.cs
+++ OopLesson03/Chapter4/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Dynamic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,27 @@ namespace Chapter4
     {
         static void Main(string[] args)
         {
+            
+        }
+        private static object GetProduct()
+        {
+            Sale sale = new Sale
+            {
+                ShopName = "pet store",
+                Amount = 100000,
+                Product = "food"
+            };
+            sale = null;
+            return sale?.Product;
+        }
+
+        class Sale
+        {
+            //店舗名
+            public string ShopName { get; set; }
+            //売上高   
+            public int Amount { get; set; }
+            public string Product { get; set; }
         }
     }
 }
