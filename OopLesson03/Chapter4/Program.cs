a99a4b26cc5628f562460c3cf4b79d361d59bbac
KeitoHoriuchi <31110@ojs.ac.jp>
2020-10-02 14:06:58 +0900

    演習問題4.1

--- OopLesson03/Chapter4/Program.cs
+++ OopLesson03/Chapter4/Program.cs
@@ -13,27 +13,8 @@ namespace Chapter4
     {
         static void Main(string[] args)
         {
-            
-        }
-        private static object GetProduct()
-        {
-            Sale sale = new Sale
-            {
-                ShopName = "pet store",
-                Amount = 100000,
-                Product = "food"
-            };
-            sale = null;
-            return sale?.Product;
-        }
 
-        class Sale
-        {
-            //店舗名
-            public string ShopName { get; set; }
-            //売上高   
-            public int Amount { get; set; }
-            public string Product { get; set; }
         }
     }
+
 }
