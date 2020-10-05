6c230c2da067fb4694f9f1c477e97b779dced593
KeitoHoriuchi <31110@ojs.ac.jp>
2020-10-05 15:04:27 +0900

    問題5.4

--- OopLesson03/Capter05/Program.cs
+++ OopLesson03/Capter05/Program.cs
@@ -10,65 +10,30 @@ namespace Capter05
     {
         static void Main(string[] args)
         {
-            Console.Write("文字列1：");
-            var st1 = Console.ReadLine();
-            Console.Write("文字列2：");
-            var st2 = Console.ReadLine();
+            //5.4
+            var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
 
-            if (string.Compare(st1, st2, ignoreCase: true) == 0) {
-                Console.WriteLine("等しい");
-            }else {
-                Console.WriteLine("等しくない");
-            }
-
-            //5.2
-            Console.WriteLine("数値文字列：");
-            var line = Console.ReadLine();
-            int num;    //返還後の数値格納用
-            if(int.TryParse(line, out num)) {
-                Console.WriteLine(num.ToString("#,#"));
-            }
-
-	    //5.3
-	    var st = "Jackdaws love my big sphinx of quartz";
-
-            //5.3.1
-            Console.WriteLine("・5.3.1");
-            int spaces = st.Count(c => c == ' ');
-            Console.WriteLine("空白数：[0]", spaces);
-
-            //5.3.2
-            Console.WriteLine("・5.3.2");
-            var replaced = st.Replace("big", "small");
-            Console.WriteLine(replaced);
-
-            //5.3.3
-            Console.WriteLine("・5.3.3");
-            int count = st.Split(' ').Count();
-            Console.WriteLine("単語数[0]", count);
-
-            //5.3.4
-            Console.WriteLine("・5.3.4");
-            //var words = st.Split(' ').Where(s => s.Length <= 4);
-            //foreach(var word in words)
-            //{
-            //    Console.WriteLine(word);
-            //}
-
-            st.Split(' ').Where(s => s.Length <= 4).ToList().ForEach(Console.WriteLine);
-
-            //5.3.5
-            Console.WriteLine("・5.3.5");
-            var array = st.Split(' ').ToArray();
-            if (array.Length > 0)
+            Console.WriteLine("・5.4");
+            var words = line.Split(';');
+            foreach(var item in words)
             {
-                var sb = new StringBuilder(array[0]);
-                foreach (var word in array.Skip(1))
-                {
-                    sb.Append(' ');
-                    sb.Append(word);
-                }
-                Console.WriteLine();
+                var st = item.Split('=');
+                Console.WriteLine("{0}:{1}", ToJapanese(st[0]), st[1]);
+            }
+        }
+
+        static string ToJapanese(string key)
+        {
+            switch (key)
+            {
+                case "Novelist":
+                    return "作家　";
+                case "BestWork":
+                    return "代表作";
+                case "Born":
+                    return "誕生年";
+                default:
+                    return "　　　";
             }
         }
     }
