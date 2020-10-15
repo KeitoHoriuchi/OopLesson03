014934a96baff22687ee28e3145b429c482bd58f
KeitoHoriuchi <31110@ojs.ac.jp>
2020-10-15 11:57:49 +0900

    辞書登録プログラムの作成

---
+++ OopLesson03/Chapter07/MonthDay.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter07{
+    class MonthDay  {
+        public int Day { get; private set; }
+        public int Month { get; private set; }
+
+        public MonthDay(int month, int day) {
+            Month = month;
+            Day = day;
+        }
+
+        //MonthDay同士の比較をする
+        public override bool Equals(object obj)
+        {
+            var other = obj as MonthDay;
+            if (other == null)
+                throw new ArgumentException();
+            return Day == other.Day && Month == other.Month;
+        }
+
+        //ハッシュコードを求める
+        public override int GetHashCode()
+        {
+            var code = Month.GetHashCode();
+            return Month.GetHashCode() * 31 + Day.GetHashCode();
+        }
+    }
+}
---
+++ OopLesson03/Chapter07/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter07{
+    class Program {
+        static void Main(string[] args)
+        {
+            //起動時に表示
+            star();
+            Console.WriteLine("* 辞書登録プログラム *");
+            star();
+
+            //単語登録用のList
+            var dic = new Dictionary<string, List<string>>();
+            do
+            {
+                Console.WriteLine("1.登録　2.内容を表示　3.終了");
+                int order = int.Parse(Console.ReadLine());
+
+                if (order == 1) {
+                    //1.単語登録
+                    Console.Write("KEYを入力：");
+                    var key = Console.ReadLine();
+                    Console.Write("VALUEを入力：");
+                    var value = Console.ReadLine();
+                    if (dic.ContainsKey(key)) {
+                        dic[key].Add(value);
+                    } else {
+                        dic[key] = new List<string> { value };
+                    }
+                    Console.WriteLine("登録しました！");
+                }                
+                else if (order == 2) {
+                    //2.内容を表示
+                    Console.WriteLine("KEY：VALUE");
+                    foreach (var item in dic) {
+                        foreach (var words in item.Value)
+                        {
+                            Console.WriteLine("{0}：{1}", item.Key, words);
+                        }
+                    }
+                }
+                else if (order == 3)
+                    break;
+            } while (true);
+        }      
+      
+        private static void star()
+        {
+            for (int i = 0; i <= 21; i++)
+                Console.Write('*');
+            Console.WriteLine();
+        }
+    }
+}
